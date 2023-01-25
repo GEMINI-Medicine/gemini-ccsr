e8a183cbb3dad4b8bc1750c4bc2991139bd8ea25
lofflerA <anne.loffler@unityhealth.to>
2023-01-25 14:37:36 -0500

    Fix bug in formatter unittest

--- tests/test_formatter.py
+++ tests/test_formatter.py
@@ -2,6 +2,7 @@ import unittest
 
 import pandas as pd
 from pandas.testing import assert_frame_equal
+import numpy as np
 
 from gemini_ccsr import formatter
 
@@ -10,7 +11,8 @@ class TestFormatter(unittest.TestCase):
     ccsr = pd.read_csv('tests/test_data/clean_ccsr_v2020-3.csv', dtype= 'str')
     ccsr = ccsr.drop(columns=['icd_description'])
 
-    icd = pd.read_csv('example_codes_to_map.csv', dtype= 'str'); 
+    icd = pd.read_csv('example_codes_to_map.csv', dtype= 'str')
+    icd = icd['diagnosis_code'].tolist()
 
     def test_check_ccsr(self):
         assert_frame_equal(
@@ -20,7 +22,7 @@ class TestFormatter(unittest.TestCase):
         self.assertEqual(len(formatter.check_icd(self.icd)), len(self.icd))
 
     def test_check_icd_list(self):
-        self.assertEqual(len(formatter.check_icd(self.icd.tolist())),
+        self.assertEqual(len(formatter.check_icd(self.icd)),
                           len(self.icd))
 
     def test_check_icd_ser(self):
